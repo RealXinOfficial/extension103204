15c902738f69461a73391fcebfb469ed7691e56d
Xin Xin McKenzie <142085035+RealXinOfficial@users.noreply.github.com>
2025-06-14 03:10:14 -0700

    JS Must be added for HouseFeed due to Link replace unsaves

---
+++ HouseFeed/HouseFeedLink.js
@@ -0,0 +1,7 @@
+function CastleCandas() {
+            window.location.href = "RXin5154852134.pdf";
+        }
+
+        function Eldento() {
+            window.location.href = "RXin336111019254.pdf";
+        }
